2e719544b233ef7bc4e9a36ca57914a0ac12f9b8
Eko Saputro <eko.saputro@sci.ui.ac.id>
2017-10-16 12:49:21 +0700

    Update and rename No1FutureInvestmentValue.java to No01FutureInvestmentValue.java

--- No01FutureInvestmentValue.java
+++ No01FutureInvestmentValue.java
@@ -1,4 +1,13 @@
-package chapter1;
+
+/**
+Write a program that reads in investment amount, annual interest rate, and number of years,
+and display the future investment value using the following formula:
+
+	futureInvestmentValue = investmentAmount x (1 + monthlyInvestmentRate)^(numberOfYears x 12)
+
+For example, if you entered amount 1000, annual interest rate 3.25%, and number of years 1,
+the future investment value is 1032.98
+**/
 
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
